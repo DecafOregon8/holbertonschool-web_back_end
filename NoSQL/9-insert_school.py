ed5f58e3b21350761fd24e5f82d517799fba53aa
DecafOregon8 <oregano148@gmail.com>
2026-04-29 03:26:42 +0000

    Add Python function to insert MongoDB document

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that inserts a new document in a collection """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Inserts a new document in a collection """
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
